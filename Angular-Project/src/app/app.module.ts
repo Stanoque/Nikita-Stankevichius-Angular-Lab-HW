ade659bbc5c981b43904e8fd2f7169b505d34f9b
Stanoque <stanoquedzhumpsh@gmail.com>
2020-07-01 19:42:54 +0300

    Corrected

--- Angular-Project/src/app/app.module.ts
+++ Angular-Project/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { PokemonHeaderComponent } from './pokemon/pokemon-header/pokemon-header.component';
-import { PokemonBodyComponent } from './pokemon/pokemon-body/pokemon-body.component';
+import { AppComponent } from './app.component';;
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonHeaderComponent,
-    PokemonBodyComponent
   ],
   imports: [
     BrowserModule

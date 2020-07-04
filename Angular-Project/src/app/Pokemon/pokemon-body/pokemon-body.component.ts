44042d1eb24af26e331f689cbb803c94c7d5a309
Stanoque <stanoquedzhumpsh@gmail.com>
2020-07-05 00:19:49 +0300

    Some bootstrap markup.

--- Angular-Project/src/app/Pokemon/pokemon-body/pokemon-body.component.ts
+++ Angular-Project/src/app/Pokemon/pokemon-body/pokemon-body.component.ts
@@ -3,19 +3,37 @@ import { POKEMONS } from '../POKEMONS';
 import { Pokemon } from '../pokemon';
 import { ToggleViewService } from '../toggle-view.service';
 
+
 @Component({
   selector: 'app-pokemon-body',
   templateUrl: './pokemon-body.component.html',
   styleUrls: ['./pokemon-body.component.css']
 })
+
+
+
 export class PokemonBodyComponent implements OnInit {
 
   pokemons = [...POKEMONS];
   
 
+  
+capitalizeFirst(string: string): string {
+
+  let workString = string.split('');
+
+  workString[0] = workString[0].toUpperCase();
+
+  return workString.join('');
+  
+}
+
+
+
   onCapture(pokemon: Pokemon): void {
     const currentPokemon = this.pokemons[this.pokemons.indexOf(pokemon)];
     currentPokemon.captured = !currentPokemon.captured;
+    console.log('Pokemon ' + this.capitalizeFirst(currentPokemon.name) + ' was' + (currentPokemon.captured ? ' captured' : ' released') + '.');
   }
 
   onDamage(pokemon: Pokemon): void {

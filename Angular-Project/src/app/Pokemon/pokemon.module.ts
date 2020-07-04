66b8218d99c26e1623b441ceb21df7191bdc1af2
Stanoque <stanoquedzhumpsh@gmail.com>
2020-07-04 23:00:30 +0300

    Invalid input removed.

--- Angular-Project/src/app/Pokemon/pokemon.module.ts
+++ Angular-Project/src/app/Pokemon/pokemon.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PokemonPageComponent } from './pokemon-page/pokemon-page.component';
 import { PokemonHeaderComponent } from './pokemon-header/pokemon-header.component';
 import { PokemonBodyComponent } from './pokemon-body/pokemon-body.component';
-import { HighDamageDirective } from './high-damage.directive';
 
 
 @NgModule({
-  declarations: [PokemonPageComponent, PokemonHeaderComponent, PokemonBodyComponent, HighDamageDirective],
+  declarations: [PokemonPageComponent, PokemonHeaderComponent, PokemonBodyComponent],
   imports: [
     CommonModule
   ],

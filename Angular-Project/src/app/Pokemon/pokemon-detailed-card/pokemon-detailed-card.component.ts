e9a3a0a038bc416fd76670e34b14e83fad43a26f
Stanoque <stanoquedzhumpsh@gmail.com>
2020-07-27 15:06:32 +0300

    DetailedCard component is created.

---
+++ Angular-Project/src/app/Pokemon/pokemon-detailed-card/pokemon-detailed-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-detailed-card',
+  templateUrl: './pokemon-detailed-card.component.html',
+  styleUrls: ['./pokemon-detailed-card.component.css']
+})
+export class PokemonDetailedCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
